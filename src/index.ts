06c65008f9002abbdda2feca4f7e83f526770a6c
absurdprofit <nahtanjohn1l@gmail.com>
2024-12-22 00:18:31 -0500

    Remove constants from exports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import 'npm:reflect-metadata@0.2.2';
 export * from './common/decorators.ts';
 export type * from './common/types.ts';
 export * from './common/utils.ts';
-export * from './common/constants.ts';
 export * from './AppServer.ts';
 export * from './Resource.ts';
 export * from './ServiceMap.ts';
