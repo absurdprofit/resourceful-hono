7ac238f2c2dc0b8a00e99bca78209e3d27a6d793
absurdprofit <nahtanjohn1l@gmail.com>
2025-01-01 16:47:22 -0500

    Refactor list.ts and moved to tasks folder

--- tasks/list.ts
+++ tasks/list.ts
@@ -1,11 +1,24 @@
 import { parseArgs } from 'jsr:@std/cli@1.0.9/parse-args';
 import { globToRegExp, isGlob, join } from 'jsr:@std/path@1.0.8';
 import { exists, expandGlob } from 'jsr:@std/fs@1.0.8';
-import DenoJSON from './deno.json' with { type: 'json' };
+import DenoJSON from '../deno.json' with { type: 'json' };
 
 async function exec(command: string, args: string[]) {
   const { stdout } = await new Deno.Command(command, { args }).output();
   return new TextDecoder().decode(stdout);
+}
+
+async function summaryFromPackage(path: string) {
+  const denoJSONPath = join(path, 'deno.json');
+  if (await exists(denoJSONPath)) {
+    const denoJSON = JSON.parse(await Deno.readTextFile(denoJSONPath));
+    return {
+      name: denoJSON.name,
+      version: denoJSON.version,
+      path
+    };
+  }
+  return null;
 }
 
 async function getAllPackages() {
@@ -13,55 +26,34 @@ async function getAllPackages() {
   for (const pattern of DenoJSON.workspace) {
     const entries = await Array.fromAsync(expandGlob(pattern));
     for (const { path } of entries.filter(entry => entry.isDirectory)) {
-      const denoJSONPath = join(path, 'deno.json');
-      if (await exists(denoJSONPath)) {
-        const denoJSON = JSON.parse(await Deno.readTextFile(denoJSONPath));
-        list.push({
-          name: denoJSON.name,
-          version: denoJSON.version,
-          path
-        });
-      }
+      list.push(await summaryFromPackage(path));
     }
   }
 
-  return list;
+  return list.filter(Boolean);
 }
 
-async function getChangedPackages(lastCommitHash: string) {
-  const output = await exec('git', ['diff', '--name-only', lastCommitHash]);
-  const changedFiles = output.trim().split('\n');
-  const projects = new Array<string>();
+async function getChangedPackages(sinceHash: string) {
+  const output = await exec('git', ['diff', '--name-only', sinceHash]);
+  const changedFilePaths = output.trim().split('\n');
+  const list = [];
 
   for (const pattern of DenoJSON.workspace) {
     if (isGlob(pattern)) {
       const regex = new RegExp(globToRegExp(pattern).source.replace(/\$$/, ''));
-      for (const path of changedFiles) {
+      for (const path of changedFilePaths) {
         const [match] = regex.exec(path) ?? [];
         if (match) {
-          projects.push(match);
+          list.push(await summaryFromPackage(match));
           break;
         }
       }
-    } else if (changedFiles.some(path => path.startsWith(pattern))) {
-      projects.push(pattern);
+    } else if (changedFilePaths.some(path => path.startsWith(pattern))) {
+      list.push(await summaryFromPackage(pattern));
     }
   }
 
-  const list = [];
-  for (const project of projects) {
-    const denoJSONPath = join(project, 'deno.json');
-    if (await exists(denoJSONPath)) {
-      const denoJSON = JSON.parse(await Deno.readTextFile(denoJSONPath));
-      list.push({
-        name: denoJSON.name,
-        version: denoJSON.version,
-        path: join(Deno.cwd(), project)
-      });
-    }
-  }
-
-  return list;
+  return list.filter(Boolean);
 }
 
 async function main() {
