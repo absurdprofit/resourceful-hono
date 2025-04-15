b2d89dc62158c4f54ac56e3853684a0fa3cfc4f8
absurdprofit <nahtanjohn1l@gmail.com>
2025-04-14 19:43:51 -0500

    Added request draining to graceful shutdown

--- packages/core/src/Application.ts
+++ packages/core/src/Application.ts
@@ -3,7 +3,7 @@ import { Resource } from './Resource.ts';
 import { type Service, ServiceMap } from './ServiceMap.ts';
 import { type Constructor, isResourceConstructor } from './common/types.ts';
 import { ErrorHandler, NotFoundHandler, TraceContext } from './middleware/index.ts';
-import { type RequestEvent, FinishEvent, ReadyEvent } from './common/events.ts';
+import { type RequestEvent, FinishEvent, ReadyEvent, type ResponseEvent } from './common/events.ts';
 import { PromiseWrapper } from './common/promise-wrapper.ts';
 import { TypedEventTarget } from './TypedEventTarget.ts';
 
@@ -12,6 +12,7 @@ export interface ApplicationEventMap {
   'finished': FinishEvent;
   'error': ErrorEvent;
   'request': RequestEvent;
+  'response': ResponseEvent;
 }
 
 export type ApplicationState = 'idle' | 'running' | 'finished';
@@ -98,7 +99,23 @@ export class Application extends TypedEventTarget<ApplicationEventMap> {
   }
 
   public finish = (): void => {
-    this.#services[Symbol.asyncDispose]()
+    new Promise<void>((resolve) => {
+      // await active request responses
+      if (Resource.activeRequests) {
+        const onResponse = (e: ResponseEvent) => {
+          if (!e.context.var.activeRequests) {
+            resolve();
+            this.removeEventListener('response', onResponse);
+          }
+        };
+        this.addEventListener('response', onResponse);
+      }
+      resolve();
+    })
+      .then(() => {
+        // cleanup services
+        return this.#services[Symbol.asyncDispose]();
+      })
       .then(() => {
         this.#finishedPromise.resolve();
         this.dispatchEvent(new FinishEvent());
--- packages/core/src/Resource.ts
+++ packages/core/src/Resource.ts
@@ -12,7 +12,7 @@ import { Application } from './Application.ts';
 import { ResourceClient } from './ResourceClient.ts';
 import { type ContentTypeHandler, ContentTypeRegistry } from './ContentTypeRegistry.ts';
 import type { ResourceClientInstance } from './index.ts';
-import { RequestEvent } from './common/events.ts';
+import { RequestEvent, ResponseEvent } from './common/events.ts';
 import { toKebabCase } from '@std/text';
 
 export interface TypedRedirectResponse<S extends HttpStatusCodes | number, __ = unknown> extends Response {
@@ -208,6 +208,7 @@ export abstract class Resource implements IResource {
   readonly #routeSchema = this.#collectParameterSchema<z.AnyZodObject>('route');
   readonly #acceptMetadata = this.#collectMethodMetadata<ContentTypes[] | undefined>(ACCEPT_METADATA_KEY);
   readonly #middlewareMetadata = this.#collectMethodMetadata<MiddlewareHandler[]>(MIDDLEWARE_METADATA_KEY);
+  static #activeRequests = Number();
 
   constructor() {
     [...this.#acceptMetadata.entries()].forEach(([method, contentTypes]) => {
@@ -385,6 +386,10 @@ export abstract class Resource implements IResource {
     return mergePath(this.parent?.pathname ?? '', this.route);
   }
 
+  public static get activeRequests() {
+    return this.#activeRequests;
+  }
+
   /**
    * Returns the route declared or inferred on the resource class.
    *
@@ -467,19 +472,25 @@ export abstract class Resource implements IResource {
   }
 
   readonly #handleRequest: Handler = async (context) => {
-    const method = context.req.method as RequestMethod;
-    const clone = this.#clone(context);
-    const methodHandler = clone[method]?.bind(clone);
-    const { parameters, issues } = await this.#collectParameters(context.req);
+    try {
+      Resource.#activeRequests++;
+      const method = context.req.method as RequestMethod;
+      const clone = this.#clone(context);
+      const methodHandler = clone[method]?.bind(clone);
+      const { parameters, issues } = await this.#collectParameters(context.req);
 
-    if (issues.length)
-      throw new BadRequestError('There were issues in your request.', { issues });
+      if (issues.length)
+        throw new BadRequestError('There were issues in your request.', { issues });
 
-    Application.instance.dispatchEvent(new RequestEvent(context));
-    if (Application.instance.state === 'idle')
-      await Application.instance.ready;
-    const response = await methodHandler?.(...parameters, context.req.raw.signal);
-    return response ?? Result(HttpStatusCodes.NoContent);
+      Application.instance.dispatchEvent(new RequestEvent(context));
+      if (Application.instance.state === 'idle')
+        await Application.instance.ready;
+      const response = await methodHandler?.(...parameters, context.req.raw.signal);
+      return response ?? Result(HttpStatusCodes.NoContent);
+    } finally {
+      context.set('activeRequests', Math.max(--Resource.#activeRequests, Number()));
+      Application.instance.dispatchEvent(new ResponseEvent(context));
+    }
   };
 
   #parseParameters(type: ParameterMetadata['type'], request: HonoRequest) {
