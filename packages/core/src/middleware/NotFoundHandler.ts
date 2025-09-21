3fbccdae7570f741b562e75776d807d2872309ab
absurdprofit <nahtanjohn1l@gmail.com>
2025-09-21 10:34:54 -0500

    Refactor NotFoundHandler; replace hardcoded error message with constant for improved maintainability

--- packages/core/src/middleware/NotFoundHandler.ts
+++ packages/core/src/middleware/NotFoundHandler.ts
@@ -3,8 +3,9 @@ import type { NotFoundHandler as INotFoundHandler } from 'hono/types';
 import { ErrorHandler } from './ErrorHandler.ts';
 
 const NOT_FOUND_ERROR = new NotFoundError();
+const NOT_FOUND_PREFIX = 'No Resource found at path: ';
 export const NotFoundHandler: INotFoundHandler = (context) => {
-  NOT_FOUND_ERROR.detail = 'No Resource found at path: ' + context.req.path;
+  NOT_FOUND_ERROR.detail = NOT_FOUND_PREFIX + context.req.path;
   return ErrorHandler(
     NOT_FOUND_ERROR,
     context
